b985e05e0a045aabf1cf4aa128c670f3f9043ecd
renedominguezpalenzuela <renedp1975@gmail.com>
2021-10-21 14:33:16 -0400

    arrglando font sizes

--- components/Content04.js
+++ components/Content04.js
@@ -11,40 +11,42 @@ import Card01 from "./Card01";
 
 export default function Content04 ({lista_cards} ) {
   return (
-<>
+        <>
 
 
-  <section>
-    <div id="blog" className="font_title mt-5 ms-4">UNSER BLOG</div>
-    <div className="font_card_texto mt-2 ms-4">NEW WORK, LEADERSHIP & MORE</div>
+          <section>
+            <div id="blog" className="font_title mt-5 ms-4">UNSER BLOG</div>
+            <div className="font_card_texto mt-2 ms-4">NEW WORK, LEADERSHIP & MORE</div>
 
-  <div className="row g-5 pb-3"> 
-   
-
-
-     {lista_cards.map (unCard=> (     
-<>
-           <div className="d-xl-none">
-                        <div  key={unCard.id} className="row d-flex justify-content-center">
-                            <div className="col-md-6">
-                                <Card01 id={unCard.id} imagen={unCard.image} titulo={unCard.title} texto={unCard.text}/>
-                            </div>
-                        </div>
-           </div>  
-          <div key={unCard.id} className="d-none d-xl-block  ">          
-            <Card03  id={unCard.id} imagen={unCard.image} titulo={unCard.title} texto={unCard.text} />     
-           </div> 
-  </>   )
-         
-     )}   
-   
+          <div className="row g-5 pb-3"> 
+          
 
 
-   </div>
-   
-</section>
+            {lista_cards.map (unCard=> (     
+              <div  key={unCard.id}>
+                        <div className="d-xl-none">
+                        {/* justify-content-center */}
+                                      <div  className="row d-flex ">
+                                          {/* <div className="col-md-6"> */}
+                                              <Card01 id={unCard.id} imagen={unCard.image} titulo={unCard.title} texto={unCard.text}/>
+                                          {/* </div> */}
+                                      </div>
+                        </div>  
+                        <div  className="d-none d-xl-block  ">          
+                          <Card03 id={unCard.id}  imagen={unCard.image} titulo={unCard.title} texto={unCard.text} />     
+                        </div> 
+              </div>  
+            )
+                
+            )}   
+          
 
-</>
+
+          </div>
+          
+        </section>
+
+        </>
   );
 }
 
