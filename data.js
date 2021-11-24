9af79ddb6527d6aadeacca29395973494867cae4
renedominguezpalenzuela <renedp1975@gmail.com>
2021-11-24 15:38:55 -0500

    tamano de imagenes

--- data.js
+++ data.js
@@ -3,18 +3,18 @@
 //cursos  
 export const crashkurse = [
 
-  {id:'1', image:'crashkurse/image1.jpeg', title:'CRASHKURS ZUM AGILE COACH', text:'Lorem ipsum dolor sit amet', link:'crashkurse01'},
-  {id:'2', image:'crashkurse/image2.jpeg', title:'CRASHKURS ZUM AGILE FACILITATOR ', text:'Consectetur adipiscing elit, sed do eiusmod tempor', link:'crashkurse02'},
-  {id:'3', image:'crashkurse/image3.jpeg', title:'CRASHKURS ZUM SCRUM MASTER', text:'Incididunt ut labore et dolore magna aliqua.',link:'crashkurse03'},  
+  {id:'1', image:'crashkurse/image1.jpg', title:'CRASHKURS ZUM AGILE COACH', text:'Lorem ipsum dolor sit amet', link:'crashkurse01'},
+  {id:'2', image:'crashkurse/image2.jpg', title:'CRASHKURS ZUM AGILE FACILITATOR ', text:'Consectetur adipiscing elit, sed do eiusmod tempor', link:'crashkurse02'},
+  {id:'3', image:'crashkurse/image3.jpg', title:'CRASHKURS ZUM SCRUM MASTER', text:'Incididunt ut labore et dolore magna aliqua.',link:'crashkurse03'},  
     
 ]
 
 //Servicios
 export const leistungen = [
-  {id:'1', image:'leistungen/coaching.jpeg', title:'COACHING', text:'Lorem ipsum dolor sit amet'},
-  {id:'2', image:'leistungen/mitbestimmung.jpeg', title:'MITBESTIMMUNG IM AGILEN KONTEXT ', text:'Consectetur adipiscing elit, sed do eiusmod tempor'},
-  {id:'3', image:'leistungen/organisationsentwicklung.jpeg', title:'ORGANISATIONSENTWICKLUNG', text:'Incididunt ut labore et dolore magna'} ,
-  {id:'4', image:'leistungen/inhouse_trainning.jpeg', title:'INHOUSE TRAINING', text:'Incididunt ut labore et dolore magna'}
+  {id:'1', image:'leistungen/coaching.jpg', title:'COACHING', text:'Lorem ipsum dolor sit amet'},
+  {id:'2', image:'leistungen/mitbestimmung.jpg', title:'MITBESTIMMUNG IM AGILEN KONTEXT ', text:'Consectetur adipiscing elit, sed do eiusmod tempor'},
+  {id:'3', image:'leistungen/organisationsentwicklung.jpg', title:'ORGANISATIONSENTWICKLUNG', text:'Incididunt ut labore et dolore magna'} ,
+  {id:'4', image:'leistungen/inhouse_trainning.jpg', title:'INHOUSE TRAINING', text:'Incididunt ut labore et dolore magna'}
 ]
 
 export const blogs = [
