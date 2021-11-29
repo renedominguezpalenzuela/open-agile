38f1e6bafa759f60bb1f237249ec2623d39ea80b
renedominguezpalenzuela <renedp1975@gmail.com>
2021-11-29 11:29:34 -0500

    botones de cards color hoober

--- components/Card01.js
+++ components/Card01.js
@@ -16,13 +16,6 @@ export default function Card01({
   return (
 <>
 
-
-
-{/* <!--Card--> */}
-          
-
-
-
   <div className="col p-4">
     <div className="card card-rounded   my_card ">
 
@@ -30,22 +23,27 @@ export default function Card01({
 
       <div className="card-body text-center w-100    ">
      
-        <p className="card-text font_card_title m-1">{titulo}</p>
+        <p className="card-text font_card_title  ">{titulo}</p>
 
-        { titulo!=undefined && titulo2!="" &&
-          <p className="card-text font_card_title2 m-1">{titulo2}</p>
+        { titulo2!=undefined && titulo2!="" &&
+          <div className="   d-flex align-items-center justify-content-center   ">
+           
+           <p className="card-text font_card_title2 m-1">{titulo2}</p>
+          </div>
         }
       </div>
 
      
-   <div className="card-footer text-center pb-4 ">
+   <div className="card-footer text-center pb-4  ">
       
-       <a href={`/${link}`} className="btn boton_card rounded-pill w-75 h-100" role="button" aria-pressed="true">
+       {/* <a href={`/${link}`} className="btn  boton_card rounded-pill w-75 h-100" role="button" aria-pressed="true"> */}
        
-          <div className="font_card_boton ps-2 pe-2 pt-2  pb-2 ms-2 me-2">Jetzt lesen...</div>
-          </a>
+          {/* <div className="font_card_boton  ps-2 pe-2 pt-2  pb-2 ms-2 me-2">Jetzt lesen...</div> */}
+       {/* </a> */}
+
+        <button type="button" className="btn  ms-2 me-2 w-75 h-100 btn-card font-btn-card rounded-pill  ">Jetzt lesen...</button>
      
-      </div>
+    </div>
 
     </div>
 
