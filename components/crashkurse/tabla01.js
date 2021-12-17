3e67e7d12cc755563c5d1a01032579f729bde04c
renedominguezpalenzuela <renedp1975@gmail.com>
2021-12-17 17:42:58 -0500

    vista mobil tabla cursos

--- components/crashkurse/tabla01.js
+++ components/crashkurse/tabla01.js
@@ -18,7 +18,17 @@ export default function Tabla01({
             
             return (
               <>
-               {fila( index, unItem.id_tabla, unItem.titulo, unItem.image, unItem.texto    )}
+              <div className="d-none ms-4 d-md-block pb-5 ">
+               {fila_dispositivo_ancho( index, unItem.id_tabla, unItem.titulo, unItem.image, unItem.texto    )}
+              </div>
+
+              <div className="d-md-none">
+               
+
+               {fila_dispositivo_estrecho( index, unItem.id_tabla, unItem.titulo, unItem.image, unItem.texto    )}
+              </div>
+
+
               </>
             );
           })}
@@ -30,7 +40,7 @@ export default function Tabla01({
 
 
 
-const fila = (index, id_tabla, titulo, img, texto) => {
+const fila_dispositivo_ancho = (index, id_tabla, titulo, img, texto) => {
   let alineado = index % 2 == 0 ? "item-derecha" : "item-izquierda";
 
   return (
@@ -39,7 +49,7 @@ const fila = (index, id_tabla, titulo, img, texto) => {
         {index % 2 == 0 ? (
           <>
           <div className=" m-5 w-75 position-celda-imagen-left ">
-            <div className="contenedor_tabla ">
+            <div className="contenedor_tabla  ">
               <div className="mask_right me-5  d-flex   justify-content-end">
                <img className="image-tabla  sombra_right" src={`${servidor_url}/img/${img}`} alt="" />
               </div>
@@ -49,7 +59,7 @@ const fila = (index, id_tabla, titulo, img, texto) => {
 
               {texto.map((unaLinea, index)=>(
                                               <>
-                                               <div className="text-start ">{unaLinea}</div>
+                                               <div className="text-start  ">{unaLinea}</div>
                                                 {unaLinea==="<br>"?"<br/>":""}
                                               </>
                          )
@@ -60,8 +70,8 @@ const fila = (index, id_tabla, titulo, img, texto) => {
           </>
         ) : (
           <>
-          <div className=" m-5 w-75 position-celda-imagen-right ">
-            <div className="contenedor_tabla">
+          <div className=" m-5 w-75 position-celda-imagen-right pb-5 ">
+            <div className="contenedor_tabla ">
               <div className="mask_left ps-3 d-flex   justify-content-start">
                <img className="image-tabla sombra_left" src={`${servidor_url}/img/${img}`} alt="" />
 
@@ -88,5 +98,45 @@ const fila = (index, id_tabla, titulo, img, texto) => {
         )}
       
     </>
+  );
+};
+
+
+
+const fila_dispositivo_estrecho = (index, id_tabla, titulo, img, texto) => {
+  let alineado = index % 2 == 0 ? "item-derecha" : "item-izquierda";
+
+  return (
+   
+      
+       
+          <>
+          <div className=" mt-5 w-100  ">
+            {/* <div className="contenedor_tabla "> */}
+
+              <div className="ps-4 tabla-titulo-estrecho text-start   ">
+                {titulo}
+              </div>
+
+              <div className=" ps-4 pe-4 pb-3  d-flex   justify-content-center">
+               <img className="image-tabla-estrecho  sombra_right" src={`${servidor_url}/img/${img}`} alt="" />
+              </div>
+            
+              <div className="mt-1 ps-4 pe-4">
+              {texto.map((unaLinea, index)=>(
+                                              <>
+                                               <div className="text-start ">{unaLinea}</div>
+                                                {unaLinea==="<br>"?"<br/>":""}
+                                              </>
+                         )
+              )}
+             </div>   
+            </div>
+            {/* </div> */}
+          </>
+       
+        
+      
+    
   );
 };
