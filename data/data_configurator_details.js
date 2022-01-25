581a0f1ebaba7c16e0bf8bee5c87643034175c98
renedominguezpalenzuela <renedp1975@gmail.com>
2022-01-25 16:26:47 -0500

    fixing vlog capitalization

--- data/data_configurator_details.js
+++ data/data_configurator_details.js
@@ -38,7 +38,7 @@ export const configurator_details = [
   {
     "id": "6",
     "botones": [ { "id": "14","texto": "Blog",   "link":"/#blog", "tipo":"1"},
-                   { "id": "15","texto": "VLog", "link":"/vlog", "tipo":"1"},
+                   { "id": "15","texto": "Vlog", "link":"/vlog", "tipo":"1"},
                    { "id": "17","texto": "Kontaktformular", "link":"", "tipo":"2"}],
   },
   {
