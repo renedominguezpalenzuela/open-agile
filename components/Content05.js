910122de549c3210970d38a1db67905b3c276dce
ruben97w33 <w33cuba@gmail.com>
2022-01-26 17:00:41 -0500

    footer, sponsor and card video

--- components/Content05.js
+++ components/Content05.js
@@ -10,17 +10,15 @@ import Card01 from "./Card01";
 //TODO: Crear reglas para cuando cambia de tamano mostrar cards normales
 
 export default function Content05({ datos, color_fondo }) {
-
- let clase_color_carrusel='color_carrusel';
- if (color_fondo==='blanco') {
-   clase_color_carrusel='color_carrusel_blanco';
- }
+  let clase_color_carrusel = "color_carrusel";
+  if (color_fondo === "blanco") {
+    clase_color_carrusel = "color_carrusel_blanco";
+  }
 
   return (
     <>
       <div id="MyCarouselTitle" className={`${clase_color_carrusel} pb-1`}>
-
-       {/* <div id="MyCarouselTitle" className="color_carrusel pb-1 ">  */}
+        {/* <div id="MyCarouselTitle" className="color_carrusel pb-1 ">  */}
         <div className=" row justify-content-center font_title_seccion_sombra   mt-2  pt-5">
           {" "}
           VLOG
@@ -39,32 +37,95 @@ export default function Content05({ datos, color_fondo }) {
             data-mdb-touch="true">
             {/* TODO: crear de forma dinamica */}
             <div className="carousel-indicators ">
-              <button type="button" data-bs-target="#MyCarousel" data-bs-slide-to="0" className="active " aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#MyCarousel" data-bs-slide-to="1" className="" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#MyCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-              <button type="button" data-bs-target="#MyCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
-              <button type="button" data-bs-target="#MyCarousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
-              <button type="button" data-bs-target="#MyCarousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
-              <button type="button" data-bs-target="#MyCarousel" data-bs-slide-to="6" aria-label="Slide 7"></button>
+              <button
+                type="button"
+                data-bs-target="#MyCarousel"
+                data-bs-slide-to="0"
+                className="active "
+                aria-current="true"
+                aria-label="Slide 1"></button>
+              <button
+                type="button"
+                data-bs-target="#MyCarousel"
+                data-bs-slide-to="1"
+                className=""
+                aria-label="Slide 2"></button>
+              <button
+                type="button"
+                data-bs-target="#MyCarousel"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+              <button
+                type="button"
+                data-bs-target="#MyCarousel"
+                data-bs-slide-to="3"
+                aria-label="Slide 4"></button>
+              <button
+                type="button"
+                data-bs-target="#MyCarousel"
+                data-bs-slide-to="4"
+                aria-label="Slide 5"></button>
+              <button
+                type="button"
+                data-bs-target="#MyCarousel"
+                data-bs-slide-to="5"
+                aria-label="Slide 6"></button>
+              <button
+                type="button"
+                data-bs-target="#MyCarousel"
+                data-bs-slide-to="6"
+                aria-label="Slide 7"></button>
             </div>
 
-            <div className="carousel-inner  alto_carrusel    ">
-
-            
-
+            <div className="carousel-inner  alto_carrusel  mt-md-5">
               {datos.map((unDato, index) => {
                 return (
-                  <div key={index} className={ unDato.id == 1 ? " carousel-item active" : "carousel-item"  }>
+                  <div
+                    key={index}
+                    className={
+                      unDato.id == 1 ? " carousel-item active" : "carousel-item"
+                    }>
                     <div className="d-xl-none  ">
                       <div className="row d-flex justify-content-center">
                         <div className="col-md-6">
-                          <Card01 imagen={unDato.image} titulo={unDato.title} texto={unDato.text} />
+                          <div className="row sombra_cards myanimacion mt-md-5 mt-lg-4 mi-card">
+                            <div className="col p-3">
+                              {unDato.link != undefined && unDato.link != "" && (
+                                <div className="ratio ratio-16x9">
+                                  <iframe
+                                    src={unDato.link}
+                                    title="YouTube video"
+                                    allowFullScreen></iframe>
+                                </div>
+                              )}
+                            </div>
+                            <div>
+                              <div className="card-body text-center">
+                                <p className="card-text font_card_title  ">
+                                  {unDato.title}
+                                </p>
+                                {/* <div className="card-footer-my-card text-center pb-4  ">
+                                  <a href="/">
+                                    <button
+                                      type="button"
+                                      className="btn  ms-2 me-2 w-75 h-100 btn-card font-btn-card rounded-pill  ">
+                                      Jetzt lesen...
+                                    </button>
+                                  </a>
+                                </div> */}
+                              </div>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
 
                     <div className="d-none d-xl-block  ">
-                      <Card02 video={unDato.link} titulo={unDato.title} texto={unDato.text} />
+                      <Card02
+                        video={unDato.link}
+                        titulo={unDato.title}
+                        texto={unDato.text}
+                      />
                     </div>
                   </div>
                 );
@@ -72,14 +133,14 @@ export default function Content05({ datos, color_fondo }) {
             </div>
 
             <button
-              className="carousel-control-prev zindex-3000"
+              className="carousel-control-prev zindex-3000 d-none d-md-block"
               type="button"
               data-bs-target="#MyCarousel"
               data-bs-slide="prev">
               <span className="carousel-control-prev-icon"></span>
             </button>
             <button
-              className="carousel-control-next zindex-3000"
+              className="carousel-control-next zindex-3000 d-none d-md-block"
               type="button"
               data-bs-target="#MyCarousel"
               data-bs-slide="next">
--- pages/vlog.js
+++ pages/vlog.js
@@ -2,25 +2,16 @@ import { servidor_url } from "../config";
 import Head from "next/head";
 import Image from "next/image";
 
-
-
 import MenuFlotante from "../components/MenuFlotante";
 import Footer from "../components/Footer";
-
-
-
-
 
 import MenuFlotanteBoton from "../components/MenuFlotanteBoton";
 // import MenuSuperior from "../components/MenuSuperior";
 
-
 import TextoBloque01 from "../components/textobloque01";
 import Image01 from "../components/crashkurse/image01";
 
-
 import Tabla01 from "../components/crashkurse/tabla01";
-
 
 import AreaSuperior from "../componentes/area_superior/AreaSuperior";
 
@@ -31,115 +22,89 @@ import Content05 from "../components/Content05";
 
 import ModalFormCookie from "../components/ModalFormCookie";
 
-
-
-
-
-
 //----------------------------------------------------------------------------------------------------------
 //           CRASHKURSE \ CRASHKURS ZUM AGILE COACH
 //----------------------------------------------------------------------------------------------------------
 
-export default function Home(
-  {vlog}
-) {
+export default function Home({ vlog }) {
+  const titulo1 = "VLOG";
+  const titulo2 = "";
+  const titulo2a = "";
 
+  const titulo3 = "VLOG";
 
+  const titulo4 = "";
 
+  const texto01 = [
+    "Ob interessante Interviews zum Thema New Work, kurze Fachbeiträge oder einfach mal ein paar spannende Impulse für die persönliche Weiterentwicklung. In diesem Vlog wirst Du regelmäßig auf dem Laufenden gehalten. Wir freuen uns auf die Arbeitswelt von morgen! Du auch? Willst Du keinen neuen Beitrag verpassen, dann abonniere unseren YouTube-Kanal „Open Agile“ und unseren Newsletter.",
+    "Interviews zu New Work & Co. ",
+    "Crashkurs Selbstorganisation in agilen Teams",
+    "Impulse für Deine persönliche Weiterentwicklung ",
+  ];
 
-const titulo1 = "VLOG";
-const titulo2 = "";
-const titulo2a = "";
+  const texto02 = [
+    "Interviews zu New Work & Co. ",
+    "Crashkurs Selbstorganisation in agilen Teams",
+    "Impulse für Deine persönliche Weiterentwicklung ",
+  ];
 
-const titulo3="VLOG";
+  const texto03 = [
+    "Ob interessante Interviews zum Thema New Work, kurze Fachbeiträge oder einfach mal ein paar spannende Impulse für die persönliche Weiterentwicklung. In diesem Vlog wirst Du regelmäßig auf dem Laufenden gehalten. Wir freuen uns auf die Arbeitswelt von morgen! Du auch? Willst Du keinen neuen Beitrag verpassen, dann abonniere unseren YouTube-Kanal „Open Agile” und unseren Newsletter.",
+  ];
 
-
-const titulo4 = "";
-
-
-const  texto01 = ["Ob interessante Interviews zum Thema New Work, kurze Fachbeiträge oder einfach mal ein paar spannende Impulse für die persönliche Weiterentwicklung. In diesem Vlog wirst Du regelmäßig auf dem Laufenden gehalten. Wir freuen uns auf die Arbeitswelt von morgen! Du auch? Willst Du keinen neuen Beitrag verpassen, dann abonniere unseren YouTube-Kanal „Open Agile“ und unseren Newsletter.",
-"Interviews zu New Work & Co. ",
-                            "Crashkurs Selbstorganisation in agilen Teams",
-                            "Impulse für Deine persönliche Weiterentwicklung "  
-];      
-
-const  texto02= ["Interviews zu New Work & Co. ",
-                            "Crashkurs Selbstorganisation in agilen Teams",
-                            "Impulse für Deine persönliche Weiterentwicklung "                            
-                           ];
-
-
-
-const texto03 = [
-
-
- "Ob interessante Interviews zum Thema New Work, kurze Fachbeiträge oder einfach mal ein paar spannende Impulse für die persönliche Weiterentwicklung. In diesem Vlog wirst Du regelmäßig auf dem Laufenden gehalten. Wir freuen uns auf die Arbeitswelt von morgen! Du auch? Willst Du keinen neuen Beitrag verpassen, dann abonniere unseren YouTube-Kanal „Open Agile” und unseren Newsletter.",
-
-];
-
-const text04_bullets = [
-   "Interviews zu New Work & Co.",
- "Crashkurs Selbstorganisation in agilen Teams",
- "Impulse für Deine persönliche Weiterentwicklung"
-]
-
-
-
-
+  const text04_bullets = [
+    "Interviews zu New Work & Co.",
+    "Crashkurs Selbstorganisation in agilen Teams",
+    "Impulse für Deine persönliche Weiterentwicklung",
+  ];
 
   return (
-<>
-  <Head>
-    <title>{titulo2}</title>
-    <meta name="description" content={titulo2} />
-    <link rel="icon" href="/favicon.ico" />
-    <script async src="js/chat.js" />
-    <script async src="js/menu.js" />
-  </Head>
+    <>
+      <Head>
+        <title>{titulo2}</title>
+        <meta name="description" content={titulo2} />
+        <link rel="icon" href="/favicon.ico" />
+        <script async src="js/chat.js" />
+        <script async src="js/menu.js" />
+      </Head>
 
-     
-  {/*Contenedor*/}
- <div id="principal" className="container-fluid g-0">
- <ModalFormCookie id={1} tiempo={10}/>
+      {/*Contenedor*/}
+      <div id="principal" className="container-fluid g-0">
+        <ModalFormCookie id={1} tiempo={10} />
 
-      <MenuFlotanteBoton />
-  
-        <AreaSuperior fondo="ajustable"  texto_parrafo_blanco_cursos={texto03} texto_parrafo_plequitas={text04_bullets}  area_gris_nueva={true}/>
+        <MenuFlotanteBoton />
 
-   
-  {/*Carrousel  */}
-        <Content05 datos={vlog} color_fondo="blanco"/>
+        <AreaSuperior
+          fondo="ajustable"
+          texto_parrafo_blanco_cursos={texto03}
+          texto_parrafo_plequitas={text04_bullets}
+          area_gris_nueva={true}
+        />
 
-    <div className="mt-5"> </div>
+        {/*Carrousel  */}
+        <Content05 datos={vlog} color_fondo="blanco" />
 
-    {/*Footer  */}
-    <Footer />
+        <div className="mt-5"> </div>
 
-</div>
-      
-    {/*Menu Lateral oculto  */} 
-  <MenuFlotante />
-</>
+        {/*Footer  */}
+        <Footer />
+      </div>
+
+      {/*Menu Lateral oculto  */}
+      <MenuFlotante />
+    </>
   );
 }
 
-
-
 //Obteniendo los datos desde el servidor
 export const getServerSideProps = async (context) => {
-
-
   const url5 = `${servidor_url}/api/vlog`;
   const res5 = await fetch(url5);
   const vlog = await res5.json();
 
-
- 
   return {
     props: {
-      
       vlog,
     },
   };
 };
-
