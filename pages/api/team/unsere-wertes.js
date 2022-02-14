f14f06b1672e8f9558d32fcd49c9dab99a70298d
Raul C. Rivero <rahulrcr98@gmail.com>
2022-02-14 13:28:12 -0400

    add unsere-werte

---
+++ pages/api/team/unsere-wertes.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  fetch("https://js-agileweb-backend.herokuapp.com/api/unsere-wertes?populate=%2A")
+    .then(response => response.json())
+    .then(data => {
+      res.status(200).json(data.data);
+    })
+    .catch(err =>
+      res.status(400).json(err)
+    )
+}
