1b6b9d17f767a6a51d09a844c55296d7ac92dbfc
ruben97w33 <w33cuba@gmail.com>
2022-02-15 10:14:34 -0500

    some fixes

--- components/Content02Cursos.js
+++ components/Content02Cursos.js
@@ -12,14 +12,25 @@ import Content02Final from "./Content02Final";
 export default function Content02Cursos({ cursos, main_page }) {
   return (
     <>
-      <div id="course" className={`${main_page? "offset-video row text-center mb-2 crash-container": "row text-center mb-3 "} offset-lateral-menu`}>
+      <div
+        id="course"
+        className={`${
+          main_page
+            ? "offset-video row text-center mb-2 crash-container"
+            : "row text-center mb-3 "
+        } offset-lateral-menu`}>
         <h3 className="font_title_seccion_sombra"> CRASHKURSE</h3>
         <h5 className="font_second_line2">
           jetzt fit machen für die Arbeitswelt von morgen
         </h5>
       </div>
 
-      <div  className="div_content02a   pb-5 pt-3 g-0">
+      <div
+        className={`${
+          main_page
+            ? "div_content02a   pb-5 pt-3 g-0"
+            : "div_content02c pb-5 pt-3 g-0"
+        }`}>
         <Card01Lista lista_cards={cursos} total_card_en_row={3} />
       </div>
     </>
