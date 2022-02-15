cdd423b91e439065e4119c8e54cb5065e7c92bc7
ruben97w33 <w33cuba@gmail.com>
2022-02-15 00:24:46 -0500

    integrando jobs ok

--- components/Card08Job.js
+++ components/Card08Job.js
@@ -4,6 +4,30 @@ import ReactMarkdown from "react-markdown";
 
 export default function Card08Job({ data }) {
   const titulo = data.attributes.title;
+  const url = "https://js-agileweb-backend.herokuapp.com";
+  const pdf = data.attributes.pdf;
+
+  const startsWithCapital = (word) => {
+    return word.charCodeAt(0) >= 65 && word.charCodeAt(0) <= 90;
+  };
+
+  const formatTitle = (titulo) => {
+    const aux = titulo.split(" ");
+    let titulo1 = aux[0];
+    let titulo2 = "";
+    for (let i = 1; i < aux.length; i++) {
+      if (startsWithCapital(aux[i])) {
+        for (let j = i; j < aux.length; j++) {
+          titulo2 += " " + aux[j];
+        }
+        break;
+      } else {
+        titulo1 += " " + aux[i];
+      }
+    }
+    return { titulo1: titulo1, titulo2: titulo2 };
+  };
+
   return (
     <>
       <div className="card myanimacion sombra_cards redondear-card pb-2">
@@ -12,20 +36,27 @@ export default function Card08Job({ data }) {
             <div className="col-12 pt-1 ps-3 pe-3   ">
               {titulo != undefined && titulo != "" && (
                 <>
-                  <div className="font1-card-job-new pt-3 pb-3">{titulo}</div>
+                  <div className="font1-card-job-new pt-3">
+                    {formatTitle(titulo).titulo1}
+                  </div>
+                  <div className="font1-card-job-new pb-3">
+                    {formatTitle(titulo).titulo2}
+                  </div>
                 </>
               )}
-
               <ReactMarkdown className="markdown bullets-jobs">
                 {data.attributes.description}
               </ReactMarkdown>
             </div>
-
             <div className="row mt-3 mb-2    g-0   d-flex justify-content-center">
               <div className="col-6 d-flex justify-content-center ">
                 <a
-                  href={"/doc/Stellenanzeige.pdf"}
-                  download="Stellenanzeige.pdf"
+                  onClick={() =>
+                    downloadEmployeeData(
+                      url + pdf.data.attributes.url,
+                      pdf.data.attributes.name
+                    )
+                  }
                   className="btn  btn-card-job font-btn-card-shop rounded-pill">
                   PDF-DOWNLOAD
                 </a>
@@ -55,19 +86,26 @@ export default function Card08Job({ data }) {
     </>
   );
 }
+const downloadEmployeeData = (path, name) => {
+  fetch(path).then((response) => {
+    response.blob().then((blob) => {
+      let url = window.URL.createObjectURL(blob);
+      let a = document.createElement("a");
+      a.href = url;
+      a.download = name;
+      a.click();
+    });
+  });
+};
 
 function procesarTextoLargo(texto) {
   return <> {texto}</>;
-
   let texto_devolver = "";
   let separador = " ";
-
   if (texto.substring(longitud_linea, longitud_linea + 1) === " ") {
     separador = "-";
   }
-
   //TODO: si coincide el guion con un espaciom no poner
-
   if (texto.length > longitud_linea) {
     return (
       <>
@@ -82,156 +120,3 @@ function procesarTextoLargo(texto) {
     return <> {texto}</>;
   }
 }
-
-// // import styles from "../styles/Home.module.css";
-
-// import Head from "next/head";
-// let ancho = 540;
-// let longitud_linea = 24;
-
-// import ModalFormJob from "./ModalFormJob";
-
-// export default function Card08Job({
-//   id,
-//   titulo1,
-//   titulo2,
-//   titulo_parrafo1,
-//   parrafo1,
-//   titulo_parrafo2,
-//   parrafo2,
-//   titulo_parrafo3,
-//   parrafo3,
-// }) {
-//   return (
-//     <>
-//       <div className="card myanimacion sombra_cards redondear-card pb-2">
-//         <div className="h-100 d-flex  align-items-center  ">
-//           <div className="row w-100 g-0 ">
-//             <div className="col-12 pt-1 ps-3 pe-3   ">
-//               {titulo1 != undefined && titulo1 != "" && (
-//                 <>
-//                   <div className="font1-card-job-new pt-3">{titulo1}</div>
-//                 </>
-//               )}
-
-//               {titulo2 != undefined && titulo2 != "" && (
-//                 <>
-//                   <div className="font1-card-job-new pt-1">{titulo2}</div>
-//                 </>
-//               )}
-
-//               {titulo_parrafo1 != undefined && titulo_parrafo1 != "" && (
-//                 <>
-//                   <div className="font2-card-job pt-2 mt-3">
-//                     {titulo_parrafo1}
-//                   </div>
-//                 </>
-//               )}
-
-//               <ul>
-//                 {parrafo1 != undefined &&
-//                   parrafo1.length > 0 &&
-//                   parrafo1.map((unaLinea, index) => (
-//                     <li key={index} className="font3-card-job bullets-jobs">
-//                       {unaLinea}
-//                     </li>
-//                   ))}
-//               </ul>
-
-//               {titulo_parrafo2 != undefined && titulo_parrafo2 != "" && (
-//                 <>
-//                   <div className="font2-card-job pt-2">{titulo_parrafo2}</div>
-//                 </>
-//               )}
-
-//               <ul>
-//                 {parrafo2 != undefined &&
-//                   parrafo2.length > 0 &&
-//                   parrafo2.map((unaLinea, index) => (
-//                     <div key={index} className="">
-//                       <li className=" font3-card-job bullets-jobs">
-//                         {unaLinea}
-//                       </li>
-//                     </div>
-//                   ))}
-//               </ul>
-
-//               {titulo_parrafo3 != undefined && titulo_parrafo3 != "" && (
-//                 <>
-//                   <div className="font2-card-job pt-2">{titulo_parrafo3}</div>
-//                 </>
-//               )}
-
-//               <ul>
-//                 {parrafo3 != undefined &&
-//                   parrafo3.length > 0 &&
-//                   parrafo3.map((unaLinea, index) => (
-//                     <li key={index} className="font3-card-job bullets-jobs">
-//                       {unaLinea}
-//                     </li>
-//                   ))}
-//               </ul>
-//             </div>
-
-//             <div className="row mt-3 mb-2    g-0   d-flex justify-content-center">
-//               <div className="col-6 d-flex justify-content-center ">
-//                 <a
-//                   href={"/doc/Stellenanzeige.pdf"}
-//                   download="Stellenanzeige.pdf"
-//                   className="btn  btn-card-job font-btn-card-shop rounded-pill">
-//                   PDF-DOWNLOAD
-//                 </a>
-//               </div>
-//               <div className="col-6      d-flex justify-content-center ">
-//                 <button
-//                   type="button"
-//                   className="btn  btn-card-job font-btn-card-shop rounded-pill  "
-//                   data-bs-toggle="modal"
-//                   data-bs-target={"#contactForm1"}>
-//                   JETZT BEWERBEN
-//                 </button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-
-//       <ModalFormJob
-//         id={1}
-//         quartal={1}
-//         titulo={"BEWERBUNG"}
-//         frase="Jetzt Kontakt aufnehmen!"
-//         day={""}
-//         link={""}
-//         job_title={titulo1 + " " + titulo2}
-//       />
-//     </>
-//   );
-// }
-
-// function procesarTextoLargo(texto) {
-//   return <> {texto}</>;
-
-//   let texto_devolver = "";
-//   let separador = " ";
-
-//   if (texto.substring(longitud_linea, longitud_linea + 1) === " ") {
-//     separador = "-";
-//   }
-
-//   //TODO: si coincide el guion con un espaciom no poner
-
-//   if (texto.length > longitud_linea) {
-//     return (
-//       <>
-//         <p>
-//           {texto.substring(0, longitud_linea)}
-//           {separador}
-//         </p>
-//         {texto.substring(longitud_linea)}
-//       </>
-//     );
-//   } else {
-//     return <> {texto}</>;
-//   }
-// }
--- pages/jobs.js
+++ pages/jobs.js
@@ -5,50 +5,14 @@ import MenuFlotanteBoton from "../components/MenuFlotanteBoton";
 import AreaSuperior from "../componentes/area_superior/AreaSuperior";
 import Card08Job from "../components/Card08Job";
 import { useState, useEffect } from "react";
-import { servidor_url } from "../config";
 import { backend_url } from "../config";
 //----------------------------------------------------------------------------------------------------------
 //           CRASHKURSE \ CRASHKURS ZUM AGILE COACH
 //----------------------------------------------------------------------------------------------------------
-
 export default function Home({ jobs }) {
-  console.log(jobs);
   const texto1 = "JOBS";
-  // const titulo2 = "Hier findest Du aktuelle Jobangebote";
   const texto2 = "HIER FINDEST DU AKTUELLE JOBANGEBOTE";
-
-  const titulo1 = "Praktikant (m/w/d)";
   const titulo2 = "Social Media & Online-Kommunikation";
-  const titulo_parrafo1 = ["Das sind Deine Stärken"];
-
-  const parrafo1 = [
-    "Praktische Erfahrungen im Bereich Social-Media & Online-Kommunikation",
-    "“Digital Native“ mit großer Social-Networks-Affinität",
-    "Fehlerfreier, stilsicherer und fachlich korrekter Schreibstil",
-    "Sicherer Umgang mit MS-Office sowie den gängigen Layout- und Grafik-Programmen",
-    "Selbstständiger, kreativer und lösungsorientierter Arbeitsstil",
-  ];
-
-  const titulo_parrafo2 = ["Spannende Aufgaben erwarten Dich"];
-
-  const parrafo2 = [
-    "Weiterentwicklung der Social-Media-Kanäle",
-    "Gemeinsame Erarbeitung und Umsetzung von Social-Media-Strategien",
-    "Unterstützung bei Video-Produktionen",
-    "Recherchieren und Texten von Postings",
-    "Unterstützung bei Präsentationen im Aufbau und Design",
-  ];
-
-  const titulo_parrafo3 = ["Warum Open Agile?"];
-
-  const parrafo3 = [
-    "Bei uns kannst Du direkt eine verantwortungsvolle Rolle mit einem hohen Maß an Autonomie und Eigenverantwortung übernehmen",
-    "Hier kannst Du Dein Potential zu 100% entfalten",
-    "Du hast einen großen strategischen und konzeptionellen Freiraum, um deine Vision des idealen Kundenerlebnisses zu verwirklichen",
-    "Regelmäßige Teamevents und Zusammenkünfte runden unsere “work from anywhere” Arbeitsweise ab",
-    "Du erhältst einen unbegrenzten Zugang zu frischem Obst, Getränken und allen Arten von Cerealien",
-  ];
-
   const [desktop_screen, setDesktop_screen] = useState(true);
   const handleResize = () => {
     let ancho_screen = window.innerWidth;
@@ -73,9 +37,6 @@ export default function Home({ jobs }) {
         <script async src="js/chat.js" />
         <script async src="js/menu.js" />
       </Head>
-
-      {/*Contenedor*/}
-
       <div id="principal" className="container-fluid g-0">
         <MenuFlotanteBoton />
 
@@ -87,7 +48,6 @@ export default function Home({ jobs }) {
           area_gris_nueva={true}
           shop={true}
         />
-
         <div className="row d-flex justify-content-center mt-5 mb-5">
           {jobs.data.map((card, key) => {
             return (
@@ -104,11 +64,11 @@ export default function Home({ jobs }) {
     </>
   );
 }
+
 export const getServerSideProps = async (context) => {
   const url5 = `${backend_url}/api/jobs`;
   const res5 = await fetch(url5);
   const jobs = await res5.json();
-
   return {
     props: {
       jobs,
