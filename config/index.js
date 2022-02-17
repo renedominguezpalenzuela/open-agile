f61af16cbfcec1b1a78eb5ceb352b04617bcb08e
Raul C. Rivero <rahulrcr98@gmail.com>
2022-02-17 01:30:40 -0400

    fixing 404

--- config/index.js
+++ config/index.js
@@ -2,11 +2,11 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const servidor_url = dev
   ? "http://localhost:3000"
-  : "https://feature-oa-17.open-agile.de/";
+  : "https://feature-oa-17.open-agile.de";
 
 export const backend_url = dev
   ? "http://localhost:3000"
-  : "https://feature-oa-17.open-agile.de/"
+  : "https://feature-oa-17.open-agile.de"
   // : "https://js-agileweb-backend.herokuapp.com/api";
 
 export const expire_cookies_in_days = 7;
