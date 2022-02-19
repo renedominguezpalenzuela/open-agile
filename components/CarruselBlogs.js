00636c104017d2cb31fbc8f2eb7404543fe4d4af
renedominguezpalenzuela <renedp1975@gmail.com>
2022-02-19 17:35:02 -0500

    R: fixing VLOG heigth in desktop

--- components/CarruselBlogs.js
+++ components/CarruselBlogs.js
@@ -18,7 +18,7 @@ export default function CarruselBlogs({ lista_cards }) {
           ))}
         </div>
 
-        <div className="carousel-inner altoCarrousel3   ">
+        <div className="carousel-inner altoCarrousel3 pt-3  ">
         
           <div className=" row  h-100 w-100 ">
             {lista_cards.map((unCard, index) =>
