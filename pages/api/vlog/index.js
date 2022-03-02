a415401cf8fa5815467e786f16d4c0a4060bb604
Raul C. Rivero <rahulrcr98@gmail.com>
2022-03-02 13:31:01 -0400

    sorted vlogs by date

--- pages/api/vlog/index.js
+++ pages/api/vlog/index.js
@@ -2,8 +2,10 @@ export default function handler(req, res) {
    fetch("https://js-agileweb-backend.herokuapp.com/api/vlogs?populate=%2A")
      .then(response => response.json())
      .then(data => {
-       
-        res.status(200).json(data.data);
+      const result = data.data.sort(function (a, b) {
+        return b.id - a.id;
+      });
+      res.status(200).json(result);
      })
      .catch(err =>
        res.status(400).json(err)
