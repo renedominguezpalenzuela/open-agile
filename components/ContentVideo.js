c2644d3e6f8d19056d8b710202d392fbb15b8f6b
Raul C. Rivero <rahulrcr98@gmail.com>
2022-03-14 16:54:30 -0400

    speed improvements

--- components/ContentVideo.js
+++ components/ContentVideo.js
@@ -3,24 +3,47 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faVolumeUp } from "@fortawesome/free-solid-svg-icons"; 
 import { faVolumeMute } from "@fortawesome/free-solid-svg-icons";
-import { useState, useRef  } from "react";
+import { useState, useRef, useEffect } from "react";
+
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      function handleResize() {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+        console.log(windowSize)
+      }
+
+      window.addEventListener("resize", handleResize);
+      handleResize();
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+  return windowSize;
+}
 
 export default function ContentVideo() {
   const [volumen, setvolumen] = useState(false);
   const botonVolumen = useRef();
+  const size = useWindowSize();
 
   const cambiar_volument = () => {
     setvolumen(!volumen);
     botonVolumen.current.muted = !botonVolumen.current.muted;
   };
 
-  
-
   return (
     <div className="mt-20 pt-md-0" style={{
       paddingTop: '100px'
     }}>
-      <div className="d-none d-md-block text-center contenedor_video">
+      {size.width >= 768 && <div className="text-center contenedor_video">
         <div className="ratio ratio-16x9 ">
           <video autoPlay loop muted playsInline ref={botonVolumen} >
             <source src={`video/Imagefilm_Open_Agile_Low1.mp4`}  type="video/mp4" />
@@ -48,7 +71,7 @@ export default function ContentVideo() {
               )}
             </div>
         </div>
-      </div>
+      </div>}
     </div>
   );
 }
