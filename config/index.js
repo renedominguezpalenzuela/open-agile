55e4c8cee6a06868ef9a162e0d5d7a2c9043a2cb
Moloko Plus <r.piper@onvertiser.de>
2022-07-07 16:38:34 +0200

    change live url

--- config/index.js
+++ config/index.js
@@ -2,16 +2,16 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const servidor_url = dev
   ? "http://localhost:3000"
-  : "https://open-agile.de";
+  : "https://my-app-xkn2w.ondigitalocean.app";
 
 export const backend_url = dev
   ? "http://localhost:3000"
-  : "https://open-agile.de";
+  : "https://my-app-xkn2w.ondigitalocean.app";
 // : "https://openagilestrapi.herokuapp.com/api";
 
 export const expire_cookies_in_days = 7;
 
-export const formEmail = dev ? "renedp1975@gmail.com" : "kontakt@open-agile.de";
+export const formEmail = dev ? "mail@robertpiper.de" : "kontakt@open-agile.de";
                                                             
 
 //  "kontakt@open-agile.de";
